eb96d59a56303ac3c68fb481270ab7f43cf34727
Ravali <ravaali03@gmail.com>
2025-10-09 12:01:51 +0000

    added comments in array

--- arrays.sh
+++ arrays.sh
@@ -3,3 +3,5 @@ MOVIES=("pushpa" "rrr" "devara")
 echo "first movie:${MOVIES[0]}"
 echo "second movie:${MOVIES[1]}"
 echo "third movie:${MOVIES[2]}"
+echo "All movies: ${MOVIES[@]}"
+#we use @ to get the list of all movies.
