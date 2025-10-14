232162741c3aa22cc8e3f45f955f30e06d739d9c
Ravali Lagisetty <ravaali03@gmail.com>
2025-10-14 12:14:36 +0530

    code added

---
+++ sql.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+userid = $(id -u)
+if [ $userid -ne 0 ]
+then
+  echo "You need to run this script as root user"
+  exit 1
+fi
+dnf list installed mysql
+if [ $? -ne 0 ]
+then
+  echo "MySQL is not installed. Installing MySQL"
+  dnf install mysql-server -y
+  if [ $? -ne 0 ]
+  then
+    echo "MySQL installation failed"
+    exit 1
+  else
+    echo "MySQL installation is successful"
+  fi
+  systemctl start mysqld
+  systemctl enable mysqld
+else
+  echo "MySQL is already installed"
+fi
+
