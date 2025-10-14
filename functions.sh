00d7b87911a3c2c7e47f155223c383be1250542d
Ravali Lagisetty <ravaali03@gmail.com>
2025-10-14 22:26:57 +0530

    GIT code added

--- functions.sh
+++ functions.sh
@@ -6,6 +6,7 @@ then
   exit 1
 fi
 dnf list installed mysql
+
 if [ $? -ne 0 ]
 then
   echo "MySQL is not installed. Installing MySQL"
@@ -23,5 +24,21 @@ else
   echo "MySQL is already installed"
 fi
 
+if [$? -ne 0 ]
+then
+  echo "Git is not installed. Installing GIT"
+  dnf install git -y
+  if [ $? -ne 0 ]
+  then
+    echo "GIT installation failed "
+    exit 1
+  else
+    echo "GIT installation is successful "
+  fi
+  systemctl start mysqld
+  systemctl enable mysqld
+else
+  echo "GIT is already installed"
+fi
 
 
