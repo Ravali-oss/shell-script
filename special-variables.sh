99e51065ec31ea11b2eb745da644fd13c85f29f7
B SRI SAI VIJITHA <201810101895@presidencyuniversity.in>
2026-02-27 08:17:06 +0530

    added file

--- special-variables.sh
+++ special-variables.sh
@@ -1,2 +1,12 @@
 #!/bin/bash/
+echo "all varaiables passed=$@"
+echo "no of variables passed=$#"
+echo "present working directory = $PWD"
+echo "home directory of the user = $HOME"
+echo "script name=$0"
+echo "which user is running the script=$USER"
+echo "process id of the current script= $$"
+sleep 90 & # sleep runs in the background and it displays the process id of sleep command
+echo "process id of last command in background = $!"
+
 
