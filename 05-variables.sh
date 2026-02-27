3496ba51baa53e64a816d4d27563e161eaa01b76
B SRI SAI VIJITHA <201810101895@presidencyuniversity.in>
2026-02-27 07:18:36 +0530

    added comments

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash/
 echo "enter username"
-read USERNAME
+read USERNAME # It reads and stores the entered data in USERNAME
 echo "entered username:$USERNAME"
 echo "enter password"
-read -s PASSWORD
+read -s PASSWORD #If we provide -s the entered data will not be displayed. 
 echo "welcome"
