f7d483bc41dc0e45274dd4576fecea81b7727335
B SRI SAI VIJITHA <201810101895@presidencyuniversity.in>
2026-02-28 22:23:17 +0530

    updated

--- delete-logs.sh
+++ delete-logs.sh
@@ -9,7 +9,7 @@ N="\e[0m"
 SOURCE_DIR="/home/ec2-user/app-logs"
 
 LOGS_FOLDER="/var/log/shellscript-logs"
-LOG_FILE=$(echo $0 | cut -d "." -f1 )
+LOG_FILE=$(echo $0 | cut -d "." -f1 ) # $0 represents name of the script being executed.
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 
